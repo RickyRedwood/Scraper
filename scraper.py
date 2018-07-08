123c55b7a1876a5b33c525c30cb0a8dc481ac179
RickyRedwood <36122874+RickyRedwood@users.noreply.github.com>
2018-07-07 23:04:22 -0500

    Add files via upload

--- scraper.py
+++ scraper.py
@@ -6,7 +6,14 @@ from datetime import date
 from datetime import timedelta
 
 
-def getData(lookup, m, d, y):
+def calcnumpages(numrecs):
+    numpages = numrecs // 20
+    if numrecs % 20 == 0:
+        numpages = numpages - 1
+    return numpages + 1
+
+
+def getData(driver, f, county, lookup, m, d, y):
     driver.get(lookup)
     driver.find_element_by_link_text("Advanced Options").click()
     driver.find_element_by_id("cmbMonth1").click()
@@ -30,16 +37,18 @@ def getData(lookup, m, d, y):
     driver.find_element_by_id("btnSubmit3").click()
     numrecs = driver.find_element_by_class_name('text-heading')
     numrecs = int(numrecs.text.split()[0])
-    numpages = numrecs // 20 + 1
-    if numrecs % 20 == 0:  # if records found = 20, we get an error in the for loop below
-        numpages = numpages - 1
-    # this for loop gets all of the rows and writes them to the file
-    for x in range(numpages):
+    numpages = calcnumpages(numrecs)
+    for x in range(1, numpages + 1):
         page = driver.page_source
-        if x >= 1:
-            driver.find_element_by_link_text(str(x + 1)).click()
-            page = driver.page_source
-        scrape(f, page)
+        if x == 1:
+            pass
+        elif x >= 2 and x <= 10:
+            driver.find_element_by_link_text(str(x)).click()
+            print('looking for link =', str(x))
+        else:
+            raise ValueError('Page has too many entries to read properly')
+        page = driver.page_source
+        scrape(f, page, county)
 
 
 def getDateRange():
@@ -51,49 +60,65 @@ def getDateRange():
     return startday, finishday
 
 
-def scrape(f, page):
+def scrape(f, page, county):
     #r = requests.get(url)
     soup = BeautifulSoup(page, 'lxml')
     table = soup.table  # find the table references
 
-    if table is None:
-        pass
-    else:
-        for br in table.find_all('br'):  # finds the <br> and replaces with newlines (for the names)
-            br.replace_with('\n')
+    with open(f, 'w') as outfile:
+        if table is None:
+            pass
+        else:
+            for br in table.find_all('br'):  # finds the <br> and replaces with newlines (for the names)
+                br.replace_with('\n')
 
-        table_rows = table.find_all('tr')  # returns a list of table rows
-        for tr in table_rows:
-            td = tr.find_all('td')  # finds the table data
-            row = [i.text for i in td]  # returns a list of data in each row from NDO. row is type list.
-            if len(row) == 5:
-                row.append(county)
-                print(row)
-                f.write(str(row) + '\n')
+            table_rows = table.find_all('tr')  # returns a list of table rows
+            for tr in table_rows:
+                td = tr.find_all('td')  # finds the table data
+                row = [i.text for i in td]  # returns a list of data in each row from NDO. row is type list.
+                if len(row) == 5:
+                    row.append(county)
+                    print(row)
+                    outfile.write(str(row) + '\n')
 
 
-months = {1: 'January', 2: 'February', 3: 'March', 4: 'April', 5: 'May', 6: 'June', 7: 'July', 8: 'August',
-          9: 'September', 10: 'October', 11: 'November', 12: 'December'}
-daysinmonth = {'January': 31, 'February': 28, 'March': 31, 'April': 30,
-               'May': 31, 'June': 30, 'July': 31, 'August': 31, 'September': 30,
-               'October': 31, 'November': 30, 'December': 30}
+def main():
+    months = {1: 'January', 2: 'February', 3: 'March', 4: 'April', 5: 'May', 6: 'June', 7: 'July', 8: 'August',
+              9: 'September', 10: 'October', 11: 'November', 12: 'December'}
+    daysinmonth = {'January': 31, 'February': 28, 'March': 31, 'April': 30,
+                   'May': 31, 'June': 30, 'July': 31, 'August': 31, 'September': 30,
+                   'October': 31, 'November': 30, 'December': 30}
 
-outputfiles = {# 31: 'Burt', 25: 'Butler', 43: 'Colfax', 24: 'Cuming',
-               28: 'Hamilton', 7: 'Madison', 46: 'Merrick', 10: 'Platte',
-               22: 'Saline', 6: 'Saunders', 16: 'Seward', 53: 'Stanton',
-               29: 'Washington', 27: 'Wayne'}
+    outputfiles =   {
+                    #31: 'Burt',
+                    #25: 'Butler',
+                    #43: 'Colfax',
+                    #24: 'Cuming',
+                    #28: 'Hamilton',
+                    7: 'Madison',
+                    #46: 'Merrick',
+                    #10: 'Platte',
+                    #22: 'Saline',
+                    #6: 'Saunders',
+                    #16: 'Seward',
+                    #53: 'Stanton',
+                    #29: 'Washington',
+                    #27: 'Wayne'
+                    }
 
-begdate, enddate = getDateRange()
-driver = webdriver.Chrome()
+    begdate, enddate = getDateRange()
+    driver = webdriver.Chrome()
 
-for county in outputfiles.keys():
-    lookup = 'http://www.nebraskadeedsonline.us/search.aspx?county=' + str(county)
-    outputfile = 'RAW ' + outputfiles[county] + '.txt'
-    with open(outputfile, 'w') as f:
-        mydate = enddate
-        while mydate >= begdate:
-            if mydate.weekday() < 5:
-                getData(lookup, months[mydate.month], str(mydate.day), str(mydate.year))
-            mydate = mydate - timedelta(days=1)
+    for county in outputfiles.keys():
+        lookup = 'http://www.nebraskadeedsonline.us/search.aspx?county=' + str(county)
+        outputfile = 'RAW ' + outputfiles[county] + '.txt'
+        with open(outputfile, 'w') as f:
+            mydate = enddate
+            while mydate >= begdate:
+                if mydate.weekday() < 5:
+                    getData(driver, outputfile, county, lookup, months[mydate.month], str(mydate.day), str(mydate.year))
+                mydate = mydate - timedelta(days=1)
 
-driver.close()
+    driver.close()
+
+main()
